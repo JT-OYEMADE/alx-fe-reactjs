7c7c8b1fe14e40e323c6632672443ba61d408de5
Tomisin <oyemadeoluwatomisin75@gmail.com>
2024-08-18 20:03:00 +0100

    0. Apply Inline CSS Styling to React Components

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,8 +1,7 @@
 function MainContent() {
   return (
     <main style={{ padding: '20px', backgroundColor: '#f0f0f0' }}>
-      <h2 style={{ color: 'darkslategray' }}>Main Content</h2>
-      {/* Your content goes here */}
+      <h2 style={{ color: 'darkslategray' }}>I love to visit New York, Paris, and Tokyo.</h2>
     </main>
 
   );
