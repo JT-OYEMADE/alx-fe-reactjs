76894c558c97a0ac5baf354f8ca1f30a8fdc82d5
Tomisin <oyemadeoluwatomisin75@gmail.com>
2024-09-22 23:10:36 +0100

    ....

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,12 +1,31 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://api.github.com/users/';
+const BASE_URL = 'https://api.github.com/search/users?q=';
 
-export const fetchUserData = async (username) => {
+export const fetchUserData = async (username, location, minRepos) => {
   try {
-    const response = await axios.get(`${BASE_URL}${username}`);
-    return response.data;
+    // Build the query based on the parameters provided
+    let query = '';
+
+    if (username) {
+      query += `user:${username}`;
+    }
+
+    if (location) {
+      query += ` location:${location}`;
+    }
+
+    if (minRepos) {
+      query += ` repos:>${minRepos}`;
+    }
+
+    // Make the API request using Axios
+    const response = await axios.get(`${BASE_URL}${query}`);
+
+    // Return the list of users
+    return response.data.items;
   } catch (error) {
+    // Handle any errors
     throw error;
   }
 };
