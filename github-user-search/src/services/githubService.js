098c2f736c8914fc79ba2e666961796d8003f0eb
Tomisin <oyemadeoluwatomisin75@gmail.com>
2024-09-22 21:42:30 +0100

    ....

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -6,13 +6,16 @@ export const fetchAdvancedUserData = async (username, location, minRepos) => {
   try {
     let query = '';
 
+    // Constructing the search query based on provided parameters
     if (username) query += `${username} in:login `;
     if (location) query += `location:${location} `;
     if (minRepos) query += `repos:>${minRepos} `;
 
+    // Making the API request
     const response = await axios.get(`${BASE_URL}?q=${query.trim()}`);
-    return response.data;
+
+    return response.data; // Return the user data
   } catch (error) {
-    throw new Error('User not found');
+    throw new Error('Error fetching users');
   }
 };
