22a6040074b01622c22d895c39750eb68205dfab
Tomisin <oyemadeoluwatomisin75@gmail.com>
2024-09-22 21:12:15 +0100

    1. Integrating the GitHub API and Creating a Basic Search Feature

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import SearchBar from './components/SearchBar';
+import SearchBar from './components/Search';
 import { fetchUserData } from './services/githubService';
 
 function App() {
